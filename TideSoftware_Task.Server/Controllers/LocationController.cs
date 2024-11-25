991bcbe5ca42664982abb608f3e003d0e7e340fc
Bartosz Truszkowski <truszkoo@gmail.com>
2024-11-25 23:45:33 +0100

    Add project files.

---
+++ TideSoftware_Task.Server/Controllers/LocationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TideSoftware_Task.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+
+    }
+}
